7ff3f57c2e027675d5892d638d36c930c4dfec59
Leonardo Viada <leonardo.viada@revo.digital>
2020-01-15 02:36:09 +0100

    high hopes

---
+++ config/keys.js
@@ -0,0 +1,23 @@
+module.exports = {
+  MongoURI:
+    'mongodb+srv://leo:stefanopapi@graffitisdb-1gj3y.mongodb.net/test?retryWrites=true&w=majority',
+  google: {
+    clientID:
+      '628325463733-f1hcffml6mrjvvb1mvamdu8834eog2nl.apps.googleusercontent.com',
+    clientSecret: 'zNzjHYvgNe0OuBdPiXfs6Usi'
+  },
+  googleUsers: {
+    clientID: 'test',
+    clientID: 'test'
+  },
+  session: {
+    cookieKey: 'oggimisentoparticolarmenteproduttivo'
+  },
+  expressMessages: 'devorispondereaunpodimessaggi',
+  mail: {
+    user: 'app@revo.digital',
+    psw: 'pasqualeciccone'
+  }
+};
+
+
